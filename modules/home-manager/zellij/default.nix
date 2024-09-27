e46fb1710f9c4f6c3c364ae76adf5e2d23b9fc81
Luke Xavier Symington <lukexaviersymington@gmail.com>
2024-09-27 17:23:37 +0100

    Zellij Colours

--- modules/home-manager/zellij/default.nix
+++ modules/home-manager/zellij/default.nix
@@ -16,8 +16,8 @@ in
         themes = {
           default = {
             fg = theme { colour = "white"; subVariant = "dim"; };
-            bg = theme { colour = "black"; };
-            black = theme { colour = "black"; subVariant = "dim"; };
+            bg = theme { colour = "grey"; subVariant = "dim"; };
+            black = theme { colour = "black"; };
             red = theme { colour = "red"; subVariant = "dim"; };
             green = theme { colour = "green"; subVariant = "dim"; };
             yellow = theme { colour = "yellow"; subVariant = "dim"; };
